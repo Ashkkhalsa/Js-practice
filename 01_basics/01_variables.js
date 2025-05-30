7b0aa4b3e218825dd95db9286fc749af38fe2b08
Ashkkhalsa <btsiarmy2017@gmail.com>
2025-05-30 08:20:30 +0000

    Add js files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId  = 144553
+let accountEmail ="aash@gmail.com"
+var accountPassword = "11133"
+accountCity = "Jaipur"
+accountState;
+
+// accountId = 2 // Not allowed
+accountEmail = "guydfdgf@gmail.com"
+accountPassword = "992992"
+accountCity = "Delhi"
+console.log(accountId);
+
+/*
+Prefer not to use war : because of issue in block scope and functional scope. {}
+
+*/
+console.table([accountId,accountEmail,accountPassword, accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";// treat all js code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3+
+    3) // Code redability should be high 
+
+console.log("Hello");
+
+let name = "ash"
+let age = 22
+let login = false
+
+// number => 2 to power 53
+//bigint (Stockmarket and all
+//string => ""
+//boolean => true/false
+//null (It is a stand alone value) intentially saying its empty
+//undefined => when value not defined but variable is given
+//symbol (unquieness find)
+
+
+
+// object
+
+console.log(typeof null); // object (said to be an js error)
+console.log(typeof undefined); // undefined 
