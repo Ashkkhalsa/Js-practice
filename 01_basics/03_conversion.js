e1ced4189e57ebfa1d98876a0f6dcd4919b7bca8
Ashkkhalsa <btsiarmy2017@gmail.com>
2025-05-30 09:24:05 +0000

    operators

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -27,8 +27,54 @@ let isloggedIn = "ash"
 let someNumber = 33
 
 let stringSomeNumber = String(someNumber)
-console.log(stringSomeNumber);
-console.log(typeof stringSomeNumber);
+// console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber);
+
+//*************************************** OPERATIONS *********************************************** */
+
+let value = 44
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3); //Remainder
+
+let str1 = "Hello"
+let str2 = " Ashmeet"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);//shouldn't ne done
+// console.log(+true); shouldn't ne done
+// // console.log(true+); shouldn't ne done
+// console.log(+""); shouldn't ne done
+
+let num1, num2, num3 //shouldn't ne done
+
+num1 = num2 = num3 = 2 + 2 //shouldn't ne done not readable
+
+let gameCounter =100
+++gameCounter; //Prefix Operater (Study in mdn)
+gameCounter++; // postfix Operater
+console.log(gameCounter);
+
+
+
+
+
+
+
 
 
 
