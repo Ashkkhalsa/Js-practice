49cdb0a60d22f492b4853c90157e1d69dbd1efc3
Ashkkhalsa <btsiarmy2017@gmail.com>
2025-05-30 15:28:16 +0000

    datatype

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,28 @@
+//js is a dynamically typed value
+//On the basis how data is stored in memory and how it is accessed . datatype is divided into 2 types.
+
+// Primitive(The value given to you is kept as it is and it gives you a copy to use)
+//7 types: String,Number,boolean,Null, Undefined,Symbol,BigInt
+
+
+
+// Non Primitive/ Reference Type
+// Array, Objects, Functions.
+
+
+
+//Return type of  value in js
+// 1) Primitive
+//  Number = Number
+//  String = String
+//  Boolean = Boolean
+//  null = Object
+//  undefined = undefined
+//  Symbol = Symbol
+//  BigInt = BigInt
+
+
+//  2) Reference Type
+//  Array = Object
+//  function = function
+//  Object = Object
