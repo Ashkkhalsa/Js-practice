2b7a48e43f6ae49735641170843d37b16bf9d6ad
Ashkkhalsa <btsiarmy2017@gmail.com>
2025-05-30 09:00:25 +0000

    conversion of data types

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,35 @@
+// let score = "33a" //Nan = not a number(speacial number.) This comeswhen you tryto make the sting into number but it actualluy has some srring as well)
+// let score = null       in null it shows 0 .
+// let score = undefined   in undefined it shoes Nan
+// let score ="ash"       in string it shows Nan
+// let score = true        in boolean it shows 0 or 1
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33"=> 33
+// "33abc" => Nan
+// true =>1 ; false =>0
+
+let isloggedIn = "ash"
+
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+// emply sting => false; filles string = true
+
+let someNumber = 33
+
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
+
+
+
+
